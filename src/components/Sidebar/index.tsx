b1287a57d0da6be0e007ebe2cd6393a3e052ee65
ebner <ebner.suporte@hotmail.com>
2023-06-06 23:57:48 -0300

    feat: add sidebar navigation

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,18 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import { useAppSelector } from '../../store/hooks';
 import { Container, SidebarItem } from './styles';
 import { AiOutlineHome, AiFillQuestionCircle } from "react-icons/ai";
 
 export default function Sidebar() {
+  const navigate = useNavigate()
   const { isSidebarOpened } = useAppSelector(state => state.app.data)
 
   return (
     <Container isOpened={isSidebarOpened}>
-      <SidebarItem>
+      <SidebarItem onClick={() => navigate('/')}>
         <AiOutlineHome   />
         Home
       </SidebarItem>
 
-      <SidebarItem>
+      <SidebarItem onClick={() => navigate('about')}>
         <AiFillQuestionCircle />
         Sobre
       </SidebarItem>
